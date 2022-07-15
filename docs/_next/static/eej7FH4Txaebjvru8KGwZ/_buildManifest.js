34c91e894d13b8530491e4d54db127063bf84c98
Avenauer <Avenauer@gmail.com>
2022-07-15 14:26:16 +0200

    Prefix

--- docs/_next/static/eej7FH4Txaebjvru8KGwZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/291-d1e20bd510531660.js","static/css/c2f71194ad41735a.css","static/chunks/pages/index-6bf0970952bf2026.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/containers/projects/ConfiguratorImages.tsx
+++ src/containers/projects/ConfiguratorImages.tsx
@@ -16,6 +16,8 @@ const ConfiguratorImages = () => {
   const y1 = useSpring(useTransform(scrollY, [0, 11000], [-200, 1500]), springConfig);
   const y2 = useSpring(useTransform(scrollY, [0, 11000], [900, -1400]), springConfig);
 
+  console.log(Imagetest2);
+
   return (
     <>
       <motion.div
@@ -28,7 +30,7 @@ const ConfiguratorImages = () => {
         }}
         className="absolute left-0 right-0 mx-auto h-96 w-full max-w-lg"
       >
-        <Image alt="Mountains" src="assets/temp/test_2.jpg" layout="fill" objectFit="contain" />
+        <Image prefix="" alt="Mountains" src={Imagetest2} layout="fill" objectFit="contain" />
       </motion.div>
       <motion.div
         style={{ y: y2, WebkitMaskPosition: "center", WebkitMaskRepeat: "no-repeat", display: "inline-block", WebkitMaskImage: `url("${Mask.src}")` }}
