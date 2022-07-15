1b112b4f4c43117c3cb29f3c0c7f726dd62ddfe7
Avenauer <Avenauer@gmail.com>
2022-07-15 14:04:43 +0200

    Please work this time

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/my-website",
-  assetPrefix: "/my-website",
-  imagesPublicPath: '/my-website/_next/static/images/',
   images: {
     loader: "akamai",
     path: ""
