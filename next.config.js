f8fc777ba99b48ced7a07d5fe7274dbb5e4d21b8
Avenauer <Avenauer@gmail.com>
2022-07-15 12:34:37 +0200

    Possible images fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/my-website",
+  basePath: "",
   assetPrefix: "/my-website",
   images: {
     loader: "akamai",
