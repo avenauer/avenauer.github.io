f724575d88c9144ec4d62fced562a734aa9a9546
Avenauer <Avenauer@gmail.com>
2022-07-15 12:59:17 +0200

    Removed assetsPath left only basePath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "",
-  assetPrefix: "/my-website",
+  basePath: "/my-website",
   images: {
     loader: "akamai",
     path: ""
