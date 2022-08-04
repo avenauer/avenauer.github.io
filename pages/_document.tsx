9143bd6c9aadb2542e62ab65bfcc3e85387d2fdc
Avenauer <Avenauer@gmail.com>
2022-08-04 15:38:18 +0200

    _document file

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+export default function Document() {
+  return (
+    <Html lang="pl">
+      <Head>
+        <title>Marcin Bieszka - portfolio</title>
+        {/*TODO: Meta description*/}
+        <meta name="description" content="Typescript Application" />
+        <link rel="apple-touch-icon" href={`%PUBLIC_URL%/my-website/logo_sm.jpg`} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="/my-website/favicon_light.ico" rel="icon" media="(prefers-color-scheme: light)" />
+        <link href="/my-website/favicon_dark.ico" rel="icon" media="(prefers-color-scheme: dark)" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Header from "../src/containers/header/header";
 import Intro from "../src/containers/sections/intro";
 import Projects from "../src/components/projects/projects";
@@ -7,25 +6,12 @@ import Clients from "../src/containers/sections/clients";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Marcin Bieszka - portfolio</title>
-        {/*TODO: Meta description*/}
-        <meta name="description" content="Typescript Application" />
-        <link rel="apple-touch-icon" href={`%PUBLIC_URL%/my-website/logo_sm.jpg`} />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-        <link href="/my-website/favicon_light.ico" rel="icon" media="(prefers-color-scheme: light)" />
-        <link href="/my-website/favicon_dark.ico" rel="icon" media="(prefers-color-scheme: dark)" />
-      </Head>
-      <div className="bg-zinc-100 duration-200 ease-smooth dark:bg-zinc-900">
-        <Header />
-        <Intro />
-        <Projects />
-          <Clients />
-      </div>
-    </>
+    <div className="bg-zinc-100 duration-200 ease-smooth dark:bg-zinc-900">
+      <Header />
+      <Intro />
+      <Projects />
+      <Clients />
+    </div>
   );
 };
 
