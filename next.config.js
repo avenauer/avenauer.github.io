db36d0943de5849b6d1804d24d2fc55429aca025
Avenauer <Avenauer@gmail.com>
2022-10-10 11:10:23 +0200

    Set standalone output

--- next.config.js
+++ next.config.js
@@ -4,10 +4,11 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   basePath: `${isProd ? "/my-website" : ""}`,
+  output: "standalone",
   images: {
     loader: "akamai",
     path: ""
-  },
+  }
 };
 
 module.exports = nextConfig;
