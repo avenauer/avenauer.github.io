ee11d7150d080430fcf8ffbabc63d7870fb16409
Avenauer <Avenauer@gmail.com>
2022-10-11 00:45:31 +0200

    Build check

---
+++ docs/_next/static/XfpjSpz0_ZOOA-Zrt_91L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/135-a4f364ff9d19b3a3.js","static/css/1cf1be2d00446234.css","static/chunks/pages/index-aea9cf39b7f7be0f.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
