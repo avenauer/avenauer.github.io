49c78cf5a1f783142b2654263202efe24f96a14e
Avenauer <Avenauer@gmail.com>
2022-10-12 12:07:35 +0200

    GA build export & id attribute added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" id="ga-script">
         {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
