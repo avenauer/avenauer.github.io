d031d28c05bd492e117efc5440bea7cb4d387983
Avenauer <Avenauer@gmail.com>
2022-10-18 15:42:29 +0200

    Build push

--- docs/_next/static/FJ2H-NDGqtnv_FNzGkB7F/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/135-a4f364ff9d19b3a3.js","static/css/ec1bcbda6954331c.css","static/chunks/pages/index-2889da356fa8ffc3.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
