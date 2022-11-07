1c4a0b3f93bbf8385163f43c91179d34eb216d64
Avenauer <Avenauer@gmail.com>
2022-11-07 14:40:32 +0100

    Mouse scroll dark theme

--- src/components/mouseScroll.tsx
+++ src/components/mouseScroll.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const MouseScroll = () => {
   return (
     <a
-      className="absolute relative left-0 block right-0 bottom-20 mx-auto h-8 w-5 rounded-full border-2 border-zinc-900 dark:border-white"
+      className="absolute relative left-0 right-0 bottom-20 mx-auto block h-8 w-5 rounded-full border-2 border-zinc-900 dark:border-white"
       aria-label="Show recent works"
       rel="noreferrer noopener"
       href="#recent_works"
@@ -11,7 +11,7 @@ const MouseScroll = () => {
       <motion.div
         animate={{ y: 8 }}
         transition={{ duration: 1, ease: "easeInOut", repeat: Infinity, repeatType: "reverse" }}
-        className="ml-1.5 mt-1.5 h-1.5 w-1 rounded-full bg-black"
+        className="ml-1.5 mt-1.5 h-1.5 w-1 rounded-full bg-zinc-900 dark:bg-white"
       />
     </a>
   );
