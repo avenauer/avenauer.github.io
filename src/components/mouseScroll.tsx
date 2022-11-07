ed1dcf19574d15cb1c992d45c35ab5ee25496b73
Avenauer <Avenauer@gmail.com>
2022-11-07 14:48:27 +0100

    Mouse scroll init animation

--- src/components/mouseScroll.tsx
+++ src/components/mouseScroll.tsx
@@ -1,8 +1,12 @@
 import { motion } from "framer-motion";
+import { animTransition, onLoadAnimation } from "../../styles/easings";
 
 const MouseScroll = () => {
   return (
-    <a
+    <motion.a
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={onLoadAnimation}
+      transition={animTransition}
       className="absolute relative left-0 right-0 bottom-20 mx-auto block h-8 w-5 rounded-full border-2 border-zinc-900 dark:border-white"
       aria-label="Show recent works"
       rel="noreferrer noopener"
@@ -13,7 +17,7 @@ const MouseScroll = () => {
         transition={{ duration: 1, ease: "easeInOut", repeat: Infinity, repeatType: "reverse" }}
         className="ml-1.5 mt-1.5 h-1.5 w-1 rounded-full bg-zinc-900 dark:bg-white"
       />
-    </a>
+    </motion.a>
   );
 };
 
