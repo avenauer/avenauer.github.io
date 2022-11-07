03a9ec694c59a227fb6f8cdfe812617c16f111b3
Avenauer <Avenauer@gmail.com>
2022-11-07 23:21:36 +0100

    Build

---
+++ docs/_next/static/HsVTh6wKetYiaEcWy7izR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/135-7c2120dedea01cd7.js","static/css/ec1bcbda6954331c.css","static/chunks/pages/index-4bceed1a80e6cac5.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
