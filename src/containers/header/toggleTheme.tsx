33ec5f0fa62c926992a9352572e4f3e9e6e5f3cf
Avenauer <Avenauer@gmail.com>
2022-11-14 15:19:31 +0100

    Small changes

--- src/containers/header/toggleTheme.tsx
+++ src/containers/header/toggleTheme.tsx
@@ -24,8 +24,8 @@ export default function ToggleTheme() {
       transition={animTransition}
       title={`Turn on ${Dark() ? "light" : "dark"} mode`}
     >
-      <div className="group-dark:hover:bg-blue-500 relative h-6 w-10 rounded-full bg-zinc-900 dark:bg-white">
-        <motion.span animate={{ y: Dark() ? 38 : -4 }} className="absolute left-0 right-0 bottom-5 mx-auto text-center text-xs font-medium">
+      <div className="group-dark:hover:bg-blue-500 relative h-6 w-10 rounded-full bg-zinc-800 dark:bg-white">
+        <motion.span animate={{ y: Dark() ? 38 : -4 }} className="opacity-90 absolute left-0 right-0 bottom-5 mx-auto text-center text-xs font-medium">
           {Dark() ? "DARK" : "LIGHT"}
         </motion.span>
         <motion.svg animate={{ x: Dark() ? 0 : 15 }} width="24" height="24">
